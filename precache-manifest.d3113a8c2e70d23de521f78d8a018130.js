b086aedd72e1c5c7098e1533a021b70d3779e28c
vishnukumavat <vishnukumawat0950@gmail.com>
2019-11-25 20:51:46 +0530

    Updates

---
+++ precache-manifest.d3113a8c2e70d23de521f78d8a018130.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fabde0253d24eed14a1ac77444b9bfae",
+    "url": "/visuals/index.html"
+  },
+  {
+    "revision": "2953cccf8b0c1bacac76",
+    "url": "/visuals/static/css/main.3d3f050d.chunk.css"
+  },
+  {
+    "revision": "538c86ae503d6edfbd27",
+    "url": "/visuals/static/js/2.bf8ab6ee.chunk.js"
+  },
+  {
+    "revision": "2953cccf8b0c1bacac76",
+    "url": "/visuals/static/js/main.b18dd1fc.chunk.js"
+  },
+  {
+    "revision": "a420c30f1af5f6e4eb7e",
+    "url": "/visuals/static/js/runtime-main.2887052c.js"
+  },
+  {
+    "revision": "537290999c4ae82854bf2f65eaf90aa1",
+    "url": "/visuals/static/media/nav4.53729099.jpg"
+  }
+]);
